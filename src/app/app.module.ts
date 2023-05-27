c3bb2b03563287bbdf354831af49506c62f3546e
pancha-alfaro <papancha.alfaro.h@gmail.com>
2023-05-27 11:56:07 -0500

    Proyecto iniciado con distribución preliminar de componentes y pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CreateaccountComponent } from './pages/createaccount/createaccount.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { PaymentComponent } from './pages/payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AvatarComponent,
+    LoginComponent,
+    CreateaccountComponent,
+    ProfileComponent,
+    ProductsComponent,
+    BlogsComponent,
+    AboutusComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
