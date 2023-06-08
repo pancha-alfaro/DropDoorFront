b79b09308949942fab393d26f1e1e3c88edcb7b0
Maryann93 <126924101+Maryann93@users.noreply.github.com>
2023-06-08 18:08:48 -0500

    Cambios en Aboutus y Products

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit{
+ 
+  @Input()  products: any[] = [
+    { name: 'McDonalds', image: 'McDonalds.jpg' },
+    { name: 'KFC', image: 'KFC.jpg' },
+    { name: 'Noa Sushi', image: 'NoaSushi.jpg' },
+    { name: 'Dominos', image: 'Dominos.jpg' },
+    { name: 'Sopitas y Frijolada', image: 'SopitasyFrijolada.jpg' },
+    { name: 'Fruteria Pepita', image: 'FruteriaPepita.jpg' },
+    // Agrega más productos aquí según tus necesidades
+  ];
 
+  
+  constructor() { }
+
+  ngOnInit() {
   }
 
+}
